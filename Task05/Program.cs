b89ce133185b71a1b9397bdd4f34813457292dc0
Татьяна <brantowatanya@yandex.ru>
2022-11-18 17:10:00 +0300

    task05 solved

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу:
+// 1. на вход принимает одно число (N)
+// 2. на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите любое натуральное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int count = number1 * 2;
+int test = number1 * 2;
+int result = 0;
+while (count >= 0)
+{
+    result = number1 - count;
+    Console.Write($"{result} ");
+    count = count - 1;
+}

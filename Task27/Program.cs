65946b5c8c2cf0f786d5aedaacba8b5250e652a4
Татьяна <brantowatanya@yandex.ru>
2022-11-30 03:02:52 +0300

    task27 solved

---
+++ Task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая
+// принимает на вход число 
+// выдаёт сумму цифр в числе
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberModul = number;
+numberModul = Math.Abs(numberModul);
+int Sum(int num)
+{
+    int sum = 0;
+    int res = 0;
+        while (num > 0)
+    {
+        res = num % 10;
+        sum = sum + res;
+        num = num / 10;
+    }
+    return sum;
+}
+int res = Sum(numberModul);
+Console.WriteLine (res);

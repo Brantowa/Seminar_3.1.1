26bddd4e885f3c8082dbfe38699183202d3b8ff8
Татьяна <brantowatanya@yandex.ru>
2022-12-14 21:25:47 +0300

    add task43

---
+++ Task43/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая: 
+// найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] InputArray(double b1, double k1, double b2, double k2)
+{
+    double x = 0;
+    double y = 0;
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    double[] array = new double[2];
+    array[0] = x;
+    array[1] = y;
+    return array;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("(");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}; ");
+    }
+    Console.Write($"{arr[arr.Length - 1]})");
+}
+
+Console.WriteLine("Введите значение ");
+Console.Write("b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double[] arr = InputArray(b1, k1, b2, k2);
+PrintArray(arr);

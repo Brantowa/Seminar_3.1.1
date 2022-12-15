545b6ef63793e0cdc623efcd1c1a90d4ce5506a7
Татьяна <brantowatanya@yandex.ru>
2022-12-16 00:16:08 +0300

    corrected task17

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -8,5 +8,5 @@
 Console.WriteLine ("Чтобы узнать, является ли данный день недели выходным, введите любое число от 1 до 7");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number == 6 ^ number == 7) System.Console.WriteLine ($"{number} день недели является выходным");
-else if (number > 7 ^ number < 1) System.Console.WriteLine ($"{number} не соответсвует никакому дню недели");
+else if (number > 7 ^ number < 1) System.Console.WriteLine ($"{number} не соответствует никакому дню недели");
 else if (number > 0 && number < 6) System.Console.WriteLine ($"{number} день недели не является выходным");
